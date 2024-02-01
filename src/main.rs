7af74bf6b9c4d9652c61ef8c5319247ad6d85a3a
Xander LMAO <xxandrr@proton.me>
2024-02-01 14:41:23 +0000

    idk if this will pause the command, wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cli;
 mod execute;
 
+use std::io::{stdin, stdout, Read, Write};
 use crate::cli::Cli;
 use clap::Parser;
 use std::process::Command;
@@ -11,4 +12,12 @@ fn main() {
     // start discord rpc
     execute::run(args.clone());
     println!("Connected!");
+    pause()
+}
+
+fn pause() { // https://www.reddit.com/r/rust/comments/8tfyof/comment/e177530/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    let mut stdout = stdout();
+    stdout.write(b"Do Ctrl + C to exit!").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
 }
